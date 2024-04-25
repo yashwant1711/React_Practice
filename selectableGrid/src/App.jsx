275e6a9a6a59af27ccabf44e880b7c6990f25cd4
yashwant <gurjaryash00@gmail.com>
2024-04-25 23:26:52 +0530

    adding_another_project

---
+++ selectableGrid/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SelectableGrid from './Components/SelectableGrid'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Selectable Grid</h1>
+      <SelectableGrid rows={15} cols={15}/>
+    </div>
+  )
+}
+
+export default App
