d41b79c5b521a4c565cda94a729e40a65e3bce19
yashwant <gurjaryash00@gmail.com>
2024-04-25 15:12:09 +0530

    adding_some_little_stuff

--- quiz/vite-project/src/components/Result.jsx
+++ quiz/vite-project/src/components/Result.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 
 function Result({userAnswer, questions, resetQuiz=()=>{}}) {
     const score = userAnswer.filter((answer) => answer).length
+    const another = userAnswer.filter((answer) => answer).length
+    console.log(another)
+    console.log(userAnswer)
   return (
     <div className='results'>
       <h2>Results</h2>
       <p>you ansered {score} out of {questions.length}</p>
-      <span onClick={resetQuiz}>Click here</span>
+      <span onClick={resetQuiz} className='reset'>Click here</span>
       <ul>
         { questions.map((question, index) => (
           <li key={index} data-correct={userAnswer[index]}>
             Q{index + 1}. {question.question}
+            {userAnswer[index] ?  <span className='correct'> Correct</span> :  <span className='wrong'> Wrong</span>}
           </li>
+          
         ))}
       </ul>
     </div>
